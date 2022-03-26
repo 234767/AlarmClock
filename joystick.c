6f7dc83c9e72048df3498c8378755e6a521337e6
Artur Pietrzak 234768 <234768@edu.p.lodz.pl>
2022-03-26 19:18:51 +0100

    Add joystick input processing

---
+++ joystick.c
@@ -0,0 +1,24 @@
+#include <lpc214x.h>
+#include "joystick.h"
+
+/// Infinite loop untill joystick has input. Returns char:
+/// u - up; d - down; l - left; r - right; c - center
+char getJoyInput(){
+	while (1) {
+		if (((IOPIN0 & KEYPIN_UP) == 0)) {
+			return 'u';
+		}
+		if (((IOPIN0 & KEYPIN_RIGHT) == 0)) {
+			return 'r';
+		}
+		if (((IOPIN0 & KEYPIN_DOWN) == 0)) {
+			return 'd';
+		}
+		if (((IOPIN0 & KEYPIN_LEFT) == 0)) {
+			return 'l';
+		}
+		if (((IOPIN0 & KEYPIN_CENTER) == 0)) {
+			return 'c';
+		}
+	}
+}
