12cafc9bc46b66d56e6b96d9fa680fe45b0f2867
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-04-03 18:14:29 +0200

    Modifications to display header

--- display.h
+++ display.h
@@ -1,8 +1,11 @@
 #include "lpc214x.h"
 #include "util.h"
 
-#define ENABLE_PIN      (1 << 24)
-#define REGISTER_SELECT (1 << 25)
+#define LCD_DATA      0x00ff0000  //P1.16-P1.23
+#define LCD_E         0x02000000  //P1.25
+#define LCD_RW        0x00400000  //P0.22
+#define LCD_RS        0x01000000  //P1.24
+#define LCD_BACKLIGHT 0x40000000  //P0.30
 
 void DisplayInit(void);
 void LcdCommand(unsigned char byte);
@@ -10,61 +13,102 @@ void LcdData(unsigned char byte);
 void SetCursor(unsigned int line, unsigned int column);
 void LcdPrint(char* str);
 
-void DisplayInit(void){
-    IODIR1 = ENABLE_PIN | REGISTER_SELECT; 
-    IOCLR1 = ENABLE_PIN | REGISTER_SELECT;
+static void
+writeLCD(uint8 reg, uint8 data)
+{
+	volatile uint8 i;
 
-    // Set pins P1.16 - P1.23 to output and clear
-    IODIR1 = 0x00FF0000;
-    IOCLR1 = 0x00FF0000;
+	if (reg == 0)
+	  IOCLR1 = LCD_RS;
+	else
+	  IOSET1 = LCD_RS;
+	  
+    IOCLR0 = LCD_RW;
+	IOCLR1 = LCD_DATA;
+	IOSET1 = ((uint32)data << 16) & LCD_DATA;
+//printf("\nwrite: %x", data);
+	
+	IOSET1 = LCD_E;
+//	osSleep(1);
+	for(i=0; i<16; i++)
+    asm volatile (" nop"); //delay 15 ns x 16 = about 250 ns delay
+	IOCLR1 = LCD_E;
+//	osSleep(1);
+	for(i=0; i<16; i++)
+    asm volatile (" nop"); //delay 15 ns x 16 = about 250 ns delay
+}
 
-    Delay();
-    LcdCommand(0x01); // clear display
-    Delay();
-    LcdCommand(0x02); // return home
-    Delay();
-    LcdCommand(0x01); // clear again
-    Delay();
-
-    LcdCommand(0b00111000); // Set 8-bit 2-line mode
+static void
+lcdBacklight(uint8 onOff)
+{
+	if (onOff == TRUE)
+	  IOSET0 = LCD_BACKLIGHT;
+	else
+	  IOCLR0 = LCD_BACKLIGHT;
 }
 
 void LcdCommand(unsigned char byte){
-    IOCLR1 = 0x00FF0000;
-    IOSET1 = byte << 16;
-
-    IOCLR1 = ENABLE_PIN | REGISTER_SELECT;
-    Delay();
-    IOSET1 = ENABLE_PIN;
-    Delay();
-    IOCLR1 = ENABLE_PIN;
-    Delay();
+    writeLCD(0, byte);
 }
 
 void LcdData(unsigned char byte){
-    IOCLR1 = 0x00FF0000;
-    IOSET1 = byte << 16;
+    writeLCD(1, byte);
+}
 
-    IOCLR1 = ENABLE_PIN;
-    Delay();
-    IOSET1 = REGISTER_SELECT;
-    Delay();
-    IOSET1 = ENABLE_PIN;
-    Delay();
-    IOCLR1 = ENABLE_PIN | REGISTER_SELECT;
-    Delay();
+void clearDisplay() {
+    LcdCommand(0x01); 
+    delay2ms();
+}
+
+void DisplayInit(void){
+	IODIR1 |= (LCD_DATA | LCD_E | LCD_RS);
+	IOCLR1  = (LCD_DATA | LCD_E | LCD_RS);
+
+	IODIR0 |= LCD_RW;
+	IOCLR0  = LCD_RW;
+	
+    IODIR0 |= LCD_BACKLIGHT;
+    IOCLR0  = LCD_BACKLIGHT;
+
+    LcdCommand(0x30);
+    delay2ms();
+    LcdCommand(0x30);
+    delay37us();
+    LcdCommand(0x30);
+    delay37us();
+
+    LcdCommand(0x38); // set 8-bit, 2 line mode
+    delay37us();
+
+    LcdCommand(0x08); // display off
+    delay37us();
+
+    clearDisplay();
+
+    LcdCommand(0x06); // cursor direction - increment, no shift
+    delay2ms();
+
+    LcdCommand(0x0c); // display on, cursor off
+    delay2ms();
+
+    LcdCommand(0x02); // cursor to home position
+    delay2ms();
 }
 
 void SetCursor(unsigned int line, unsigned int column){
+    if (column > 39)
+        column = 39;
+
     if (line == 0)
-        LcdCommand(0x80);
+        LcdCommand(0x80 + column);
     else 
-        LcdCommand(0xc0);
+        LcdCommand(0xc0 + column);
 }
 
 void LcdPrint(char* str){
     while(*str){
         LcdData(*str);
+        delay37us();
         str++;
     }
 }
--- util.h
+++ util.h
@@ -1,3 +1,30 @@
-void Delay(void) {
-    for(int i=0; i<100; i++);
+#define TRUE    1
+#define FALSE   0
+#ifndef NULL
+#define NULL (0)
+#endif
+
+typedef unsigned char   uint8;
+typedef char            int8;
+typedef unsigned short  uint16;
+typedef short           int16;
+typedef unsigned int    uint32;
+typedef int             int32;
+
+static void
+delay37us(void)
+{
+	volatile uint8 i;
+
+	for(i=0; i<2500; i++)
+        asm volatile (" nop"); //delay 15 ns x 2500 = about 37 us delay
+}
+
+static void
+delay2ms(void)
+{
+	volatile uint8 i;
+
+	for(i=0; i<55; i++)
+        delay37us(); //delay 37 us x 55 = about 2 ms delay
 }
