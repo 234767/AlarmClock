e985f2699b8328fbe88cd33c0f014c80bf04b5ca
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-04-08 09:30:11 +0200

    dziala ale nie odlicza czasu

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
+#include "lpc2xxx.h"
 #include "timer.h"
 #include "display.h"
-#include "joystick.h"
-
+#include "joytick.h"
+#include "timer2.h"
+#include "general.h"
+#include <lpc2xxx.h>
+#include <printf_P.h>
+#include <printf_init.h>
+#include <consol.h>
+#include <config.h>
+#include "irq/irq_handler.h"
+#include "timer.h"
+#include "VIC.h"
+ 
+#include "Common_Def.h"
+ 
 struct time {
     uint8 hour;
     uint8 minute;
     uint8 second;
 };
 typedef struct time Time;
-
+ 
 Time currentTime;
 uint8 led12;
-uint8 mode; 
+uint8 mode;
 /*
 0 - normal
 1 - setting hour
 2 - setting minute
 3 - setting second
 */
-
+ 
 static void displayTime(){
     SetCursor(0,0);
     LcdData((currentTime.hour / 10) + 0x30);
@@ -30,7 +43,7 @@ static void displayTime(){
     LcdData((currentTime.second / 10) + 0x30);
     LcdData((currentTime.second % 10) + 0x30);
 }
-
+#if 0
 static void onTimerFire(void) {
     if (led12 == 0) {
         IOCLR0 = (1 << 12);
@@ -40,7 +53,7 @@ static void onTimerFire(void) {
         IOSET0 = (1 << 12);
         led12 = 0;
     }
-
+ 
     if (mode != 0) {
         return;
     }
@@ -57,30 +70,36 @@ static void onTimerFire(void) {
             }
         }
     }
-
+ 
     //display current time
     displayTime();
 }
-
-void init_irq (int32 period, int8 duty_cycle) {
-    VICIntSelect &= ~0x20;
-    VICVectAddr5 = (int32)onTimerFire;
-    VICVectCntl5  = 0x25;            //enable vector interrupt for timer1 
-    VICIntEnable  = 0x20;            //enable timer1 interrupt
-  
-    //initialize and start Timer #1
-    T1TCR = 0x00000002;                           //disable and reset Timer1
-    T1PC  = 0;                                    // no prescale
-    T1MR0 = ((unsigned long long) period ) * ((unsigned long long) CRYSTAL_FREQUENCY )/1000;
-    T1MR1 = (unsigned long long)T1MR0 * duty_cycle / 100;
-    T1IR  = 0x000000ff;                           //reset all flags before enable IRQs
-    T1MCR = 0x00000003;                           //reset counter and generate IRQ on MR0 match
-    T1TCR = 0x00000001;   
+ 
+static void init_irq (tU32 period, tU8 duty_cycle)
+{
+    clearDisplay();
+    LcdPrint("InitIRQ");
+    //Zainicjuj VIC dla przerwań od Timera #1
+    VICIntSelect &= ~TIMER_1_IRQ;           //Przerwanie od Timera #1 przypisane do IRQ (nie do FIQ)
+    VICVectAddr5  = (tU32)onTimerFire;         //adres procedury przerwania
+    VICVectCntl5  = VIC_ENABLE_SLOT | TIMER_1_IRQ_NO;
+    VICIntEnable  = TIMER_1_IRQ;            // Przypisanie i odblokowanie slotu w VIC od Timera #1
+ 
+    T1TCR = TIMER_RESET;                    //Zatrzymaj i zresetuj
+    T1PR  = 0;                              //Preskaler nieużywany
+    T1MR0 = ((tU64)period)*((tU64)PERIPHERAL_CLOCK)/1000;
+    T1MR1 = (tU64)T1MR0 * duty_cycle / 100; //Wypełnienie
+    T1IR  = TIMER_ALL_INT;                  //Resetowanie flag przerwań
+    T1MCR = MR0_I | MR1_I | MR0_R;          //Generuj okresowe przerwania dla MR0 i dodatkowo dla MR1
+    T1TCR = TIMER_RUN;                      //Uruchom timer
+    clearDisplay();
+    LcdPrint("pomocyaaaaa");
+    return;
 }
-
+#endif
 void handleModeO(){
     if(isButtonPressed()){
-        IO0SET = (1 << 13);
+        IOSET0 = (1 << 13);
         clearDisplay();
         displayTime();
         SetCursor(1,0);
@@ -89,15 +108,17 @@ void handleModeO(){
             LcdPrint("Time set");
             cursor_enable(1);
             mode = 1;
+            while(isButtonPressed())
+                ;
         }
         else {
             LcdPrint("Pres longer");
         }
         delay2ms();
-        IO0CLR = (1 << 13);
+        IOCLR0 = (1 << 13);
     }
 }
-
+ 
 void increment() {
     switch (mode)
     {
@@ -116,9 +137,9 @@ void increment() {
         break;
     }
 }
-
+ 
 void decrement() {
-    switch (mode) 
+    switch (mode)
     {
     case 1:
         if (currentTime.hour == 0)
@@ -142,11 +163,15 @@ void decrement() {
         break;
     }
 }
-
+ 
 void handleTimeSet() {
     if (isButtonPressed()) {
         mode = 0;
         cursor_enable(0);
+        clearDisplay();
+        displayTime();
+        while(isButtonPressed())
+            ;
         return;
     }
     char input = getYoyInput();
@@ -169,18 +194,44 @@ void handleTimeSet() {
     default:
         break;
     }
-    delayMs(500,0);
+    displayTime();
+    delayMs(1000,0);
 }
-
+ 
+static void sdelay (tU32 seconds)
+{
+    T0TCR = TIMER_RESET;                    //Zatrzymaj i zresetuj
+    T0PR  = 12000000-1;             //jednostka w preskalerze
+    T0MR0 = seconds;
+    T0IR  = TIMER_ALL_INT;                  //Resetowanie flag przerwaĹ„
+    T0MCR = MR0_S;                          //Licz do wartości w MR0 i zatrzymaj się
+    T0TCR = TIMER_RUN;                      //Uruchom timer
+ 
+    // sprawdź czy timer działa
+    // nie ma wpisanego ogranicznika liczby pętli, ze względu na charakter procedury
+    // proszę jednak pamiętać, że to jest wyjątek od reguły!
+    while (T0TCR & TIMER_RUN)
+    {
+    }
+}
+ 
 int main(void) {
     led12 = 0;
     IODIR0 |= ((1 << 12) | (1<<13));
     currentTime.hour = 0;
     currentTime.minute = 0;
     currentTime.second = 0;
+    mode = 0;
     DisplayInit();
+    lcdBacklight(1);
+    sdelay(1);
+    LcdPrint("Siema2");
+    sdelay(3);
     // startTimer1(10, (uint32) onTimerFire); // every 1 second
-    init_irq(1000,500);
+    //init_irq(1000,00);
+    clearDisplay();
+    LcdPrint("Startujemy");
+    sdelay(1);
     while(1){
         if (mode == 0) {
             handleModeO();
