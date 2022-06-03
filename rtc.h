084a1ea84e07b0d1908d944919dde251880d43fb
ArtJSON <artur.pietrzak1@outlook.com>
2022-06-03 02:00:14 +0200

    Add my part of documentation

--- rtc.h
+++ rtc.h
@@ -3,22 +3,6 @@
 #include <stdio.h>
 #include "util.h"
 
-/*
-// If needed struct can also contain other values
-
-typedef struct
-{
-	uint8 seconds;    // 0 - 59
-	uint8 minutes;    // 0 - 59  
-	uint8 hours;      // 0 - 23
-	uint8 dayOfMonth; // 1 - (28, 29, 30, 31 depending on month)
-	uint8 dayOfWeek;  // 0 - 6
-	uint16 dayOfYear; // 1 - 365 (365 for leap year)
-	uint8 month;      // 1 - 12
-	uint16 year;      // 0 - 4095
-}RTC_Time;
-*/
-
 typedef struct
 {
 	uint8 seconds;
@@ -31,36 +15,13 @@ void RTC_SetTime(RTC_Time Time)
 	SEC = Time.seconds;
 	MIN = Time.minutes;
 	HOUR = Time.hours;
-/*
-	// setting time for other values if extended struct needed
-	DOM = Time.dayOfMonth;
-	DOW = Time.day_of_weedayOfWeekk;
-	DOY = Time.dayOfYear;
-	MONTH = Time.month;
-	YEAR = Time.year;
-*/
 }
-
-/*
-	Alarm register group
-
-	If all alarm registers match time registers, interrupt is generated
-
-*/
 
 void RTC_Set_AlarmTime(RTC_Time AlarmTime)
 {
 	ALSEC = AlarmTime.seconds;
 	ALMIN = AlarmTime.minutes;
 	ALHOUR = AlarmTime.hours;
-	/*
-	// setting alarm time for other values if extended struct needed
-	ALDOM = Time.dayOfMonth;
-	ALDOW = Time.day_of_weedayOfWeekk;
-	ALDOY = Time.dayOfYear;
-	ALMONTH = Time.month;
-	ALYEAR = Time.year;
-*/
 }
 
 RTC_Time RTC_GetTime(void)
@@ -70,14 +31,6 @@ RTC_Time RTC_GetTime(void)
 	time.seconds = SEC;
 	time.minutes = MIN;
 	time.hours = HOUR;
-	/*
-	// returning time for other values if extended struct needed
-	time.dayOfMonth = DOM;
-	time.day_of_weedayOfWeekk = DOW;
-	time.dayOfYear = DOY;
-	time.month = MONTH;
-	time.year = YEAR;
-	*/
 
 	return time;
 }
@@ -89,33 +42,6 @@ RTC_Time RTC_GetAlarmTime(void)
 	AlarmTime.seconds = ALSEC;
 	AlarmTime.minutes = ALMIN;
 	AlarmTime.hours = ALHOUR;
-	/*
-	// returning alarm time for other values if extended struct needed
-	AlarmTime.dayOfMonth = ALDOM;
-	AlarmTime.day_of_weedayOfWeekk = ALDOW;
-	AlarmTime.dayOfYear = ALDOY;
-	AlarmTime.month = ALMONTH;
-	AlarmTime.year = ALYEAR;
-	*/
-
-	return AlarmTime;
-}
-
-RTC_Time RTC_Init(void)
-{
-	RTC_Time AlarmTime;
-
-	AlarmTime.seconds = ALSEC;
-	AlarmTime.minutes = ALMIN;
-	AlarmTime.hours = ALHOUR;
-	/*
-	// returning alarm time for other values if extended struct needed
-	AlarmTime.dayOfMonth = ALDOM;
-	AlarmTime.dayOfWeek = ALDOW;
-	AlarmTime.dayOfYear = ALDOY;
-	AlarmTime.month = ALMONTH;
-	AlarmTime.year = ALYEAR;
-	*/
 
 	return AlarmTime;
 }
